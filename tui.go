a9c2b01e47c648ef16c1ee79c8418b7552a1dd90
Dang Hoang Tuan (Wumi4) <danghoangtuan526@gmail.com>
2021-09-06 18:42:23 +0700

    Fancier cursor

--- tui.go
+++ tui.go
@@ -156,7 +156,7 @@ func (m Model) View() string {
 		// Is the cursor pointing at this title?
 		cursor := " " // No cursor
 		if m.cursor == i {
-			cursor = ">" // Yes cursor!
+			cursor = "⏵ " // Yes cursor!
 		}
 
 		// Render the row
